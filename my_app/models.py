e7353f87e03f08ac6c2fb5ede89a0ac7e6575e81
zora <zrnaumo@gmail.com>
2025-04-29 06:32:34 -0400

    added content

---
+++ my_app/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100,unique=True)
+    phone=models.CharField(max_length=15)
+    password=models.CharField(max_length=100)
+    weight=models.IntegerField(default=0)
+    height=models.IntegerField(default=0)
+    gender=models.CharField(max_length=100,default='male''female')
+
+
+
+    class Meta:
+        db_table ='customer'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  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)
+
+
+# python manage.py makemigrations
+#python manage.py migrate
